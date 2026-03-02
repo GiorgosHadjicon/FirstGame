823fccf98f68f7a8e4d73506af5dc6e9b9e9c923
Giorgos Hadjiconstantis <georgehadjiconstantis@gmail.com>
2026-03-02 15:54:28 +0000

    Created Game Loop & and added Key handler to read 'w,a,s,d' inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -17,13 +19,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN;
     final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
 
+    // FPS
+    int FPS = 60;
+
     Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
+
+    // Set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -33,6 +46,48 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        
+
+        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if ( delta >= 1) {
+                update();
+                repaint(); // calls paintComponent()
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+        if (keyH.upPressed == true) {
+            playerY -= playSpeed;
+        }
+        else if (keyH.downPressed == true) {
+            playerY += playSpeed;
+        }
+        else if (keyH.leftPressed == true) {
+            playerX -= playSpeed;
+        }
+        else if (keyH.rightPressed == true) {
+            playerX += playSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
+        g2.dispose();
     }
 }
